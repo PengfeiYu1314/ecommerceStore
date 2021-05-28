a22dbf1fae9822348f4c433a7de72b11e7a0606e
PengfeiYu1314 <pengfeiyu1314@gmail.com>
2021-05-28 15:18:40 -0400

    add user reducer

--- client/src/store.js
+++ client/src/store.js
@@ -3,17 +3,23 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userReducer } from './reducers/userReducers'
+
 
 const reducer = combinReducers( { 
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userReducer,
 } )
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage}
+    cart: { cartItems: cartItemsFromStorage},
+    userLogin: { userInfo: userInfoFromStorage}
 }
 
 const middleware = [thunk]

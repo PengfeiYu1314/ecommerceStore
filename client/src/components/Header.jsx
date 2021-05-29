12668972d1b805005af0feab0985b9d59a1fb43d
PengfeiYu1314 <pengfeiyu1314@gmail.com>
2021-05-29 02:21:37 -0400

    client side user auth and profile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,41 @@
 
 import {LinkContainer} from 'react-router-bootstrap'
-import {Container, Navbar, Nav} from 'react-bootstrap';
+import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userAction'
+
 
  const Header = () => {
+   const useLogin = useSelector(state => state.useLogin)
+   const {userInfo } = userLogin
+   const dispatch = useDispatch()
+  
+   const logoutHandler = () => {
+      dispatch(logout())
+   }
+
     return (
         <Navbar bg="light" expand="lg" variant='light' collapseOnSelect>
         <Container>
             <LinkContainer to='/'>
             <Navbar.Brand>E-Shop</Navbar.Brand>
             </LinkContainer>
+
             
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto" >
               <LinkContainer to='/cart'> <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link> </LinkContainer>
-              <LinkContainer to='/login'> <Nav.Link> <i className="fas fa-user"></i>Sign In</Nav.Link>  </LinkContainer>
+
+              {userInfo ? ( <NavDropdown title={userInfo.name}>
+                <LinkContainer to='/profile'>
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item  onClick={logoutHandler}>Logout</NavDropdown.Item>
+              </NavDropdown>
+
+) :  <LinkContainer to='/login'> <Nav.Link> <i className="fas fa-user"></i>Sign In</Nav.Link>  </LinkContainer> } 
+              
                 
             </Nav>
             
--- client/src/store.js
+++ client/src/store.js
@@ -3,14 +3,15 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 
 
 const reducer = combinReducers( { 
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
-    userLogin: userReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
 } )
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []

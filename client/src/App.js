dcb6678afe5871cbe56fd85e2c020456d6dc10f1
PengfeiYu1314 <pengfeiyu1314@gmail.com>
2021-05-30 00:47:05 -0400

    set up checkout process

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,9 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/RegisterScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 
 function App() {
@@ -18,11 +21,14 @@ function App() {
     <main className="py-3">
       <Container>
         <Route path='/profile' compoent={ProfileScreen} />
+        <Route path='/placeorder' compoent={PlaceOrderScreen} />
         <Route path='/login' component={LoginScreen} />
         <Route path='/register' component={RegisterScreen} />
         <Route path='/' component={HomeScreen} exact />
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
+        <Route path='/shipping' component={ShippingScreen} />
+        <Route path='/payment' component={PaymentScreen} />
       </Container>
      
     </main>

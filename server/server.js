982320ad6f605c2436d5b020d49cb2edfcb34923
PengfeiYu1314 <pengfeiyu1314@gmail.com>
2021-06-03 21:54:53 -0400

    prepare to heroku deploy

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
-import { notFound, errorHandler } from './middleware/errorHandler.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -38,14 +38,20 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.get('*', (req, res) =>
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+  })
 } else {
   app.get('/', (req, res) => {
     res.send('API is running....')
-  })
-}
+})}
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build'))) }
+
+  
 
 app.use(notFound)
 app.use(errorHandler)
